0624171293516ef4923ff02d4996a4b04926b474
rodrigoreisdev <67196386+rodrigoreisdev@users.noreply.github.com>
2021-02-09 14:49:21 -0300

    1.0

--- src/app/detail-prod/detail-prod.component.ts
+++ src/app/detail-prod/detail-prod.component.ts
@@ -12,12 +12,12 @@ export class DetailProdComponent implements OnInit {
   item: any;
   id_prod?: any;
 
-  constructor(private ApiService:ApiService, private route: ActivatedRoute) { }
+  constructor(private apiService:ApiService, private route: ActivatedRoute) { }
   
 
   ngOnInit() {
     this.route.params.subscribe(param =>{this.id_prod = param.id})
-    this.ApiService.GetListProdDetail(this.id_prod).subscribe(data => { this.item = data })
+    this.apiService.GetListProdDetail(this.id_prod).subscribe(data => { this.item = data})
     //this.ApiService.GetListProd().subscribe(data => { this.product = data })
   }
 
